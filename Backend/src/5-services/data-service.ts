31f21b6a43ff2e655eaec1651d1fe97cf6118ddb
Itay <itay3742@gmail.com>
2023-04-28 20:11:49 +0300

    Update data-service.ts

--- Backend/src/5-services/data-service.ts
+++ Backend/src/5-services/data-service.ts
@@ -1,12 +1,42 @@
+import UserModel from "../2-models/user-model";
+import VacationModel from "../2-models/vacation-model";
 import dal from "../4-utils/dal";
 import { OkPacket } from "mysql";
 
-async function getAll____() {
+async function getAllVacations(): Promise<VacationModel[]> {
+    const sql = "SELECT * FROM vacation_table";
+    const vacations = await dal.execute(sql);
+    return vacations;
 
 }
 
+async function getAllUsers(): Promise<UserModel[]> {
+    const sql = "SELECT * FROM users_table";
+    const users = await dal.execute(sql);
+    return users;
+
+}
+ // will be used later with authentication
+async function getVacations(userId: number): Promise<VacationModel[]> {
+    const sql = `
+        SELECT DISTINCT
+            V.*,
+            EXISTS(SELECT * FROM followers_table WHERE vacationId = F.vacationId AND userId = ?) AS isFollowing,
+            COUNT(F.userId) AS followersCount
+        FROM vacation_table as V LEFT JOIN followers_table as F
+        ON V.vacationId = F.vacationId
+        GROUP BY vacationId
+        ORDER BY startDate
+        `;
+    
+    const vacations = await dal.execute(sql, [userId]);
+
+    return vacations;
+}
 
 export default {
-    getAll____
+    getAllVacations,
+    getAllUsers,
+    getVacations
 };
 
