690b453102512adeee26b0f5139e57236d48d6a5
Itay <itay3742@gmail.com>
2023-04-28 20:12:11 +0300

    Update data-routes.ts

--- Backend/src/6-routes/data-routes.ts
+++ Backend/src/6-routes/data-routes.ts
@@ -1,10 +1,33 @@
 import express, { Request, Response, NextFunction } from "express";
+import dataService from "../5-services/data-service";
 
 const router = express.Router();
 
-router.get("/____", async (request: Request, response: Response, next: NextFunction) => {
+router.get("/vacations", async (request: Request, response: Response, next: NextFunction) => {
     try {
-        // ...
+        const vacations = await dataService.getAllVacations()
+        response.json(vacations)
+    }
+    catch(err: any) {
+        next(err);
+    }
+});
+ // will be used later with authentication
+router.get("/vacations/:id", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const userId = + request.params.id
+        const vacations = await dataService.getVacations(userId)
+        response.json(vacations)
+    }
+    catch(err: any) {
+        next(err);
+    }
+});
+
+router.get("/users", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const users = await dataService.getAllUsers()
+        response.json(users)
     }
     catch(err: any) {
         next(err);
