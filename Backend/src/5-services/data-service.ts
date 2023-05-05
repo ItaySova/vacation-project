22617b4450e6dc47945a60891a4f5c2ed3303915
Itay <itay3742@gmail.com>
2023-05-05 15:24:58 +0300

    Update data-service.ts

--- Backend/src/5-services/data-service.ts
+++ Backend/src/5-services/data-service.ts
@@ -1,7 +1,9 @@
 import UserModel from "../2-models/user-model";
+import { ResourceNotFoundError, ValidationError } from "../2-models/client-errors";
 import VacationModel from "../2-models/vacation-model";
 import dal from "../4-utils/dal";
 import { OkPacket } from "mysql";
+import FollowerModel from "../2-models/follower-model";
 
 async function getAllVacations(): Promise<VacationModel[]> {
     const sql = "SELECT * FROM vacation_table";
@@ -16,6 +18,8 @@ async function getAllUsers(): Promise<UserModel[]> {
     return users;
 
 }
+
+
  // will be used later with authentication
 async function getVacations(userId: number): Promise<VacationModel[]> {
     const sql = `
@@ -34,9 +38,100 @@ async function getVacations(userId: number): Promise<VacationModel[]> {
     return vacations;
 }
 
+async function getSingleVacation(id:number): Promise<VacationModel> {
+    const sql = `SELECT * FROM vacation_table WHERE vacationId = ${id}`
+    const vacations = await dal.execute(sql);
+    const vacation = vacations[0];
+    if (!vacation) throw new ResourceNotFoundError(id);
+    return vacation;
+}
+
+async function addVacation(vacation:VacationModel): Promise<VacationModel>{
+    // validations
+    const errors = vacation.validatePost();
+    if (errors) throw new ValidationError(errors);
+
+    const sql = `INSERT INTO vacation_table(destination,description,startDate,endDate,price,pictureName)
+    VALUES ('${vacation.destination}', '${vacation.description}',
+            '${vacation.startDate}', '${vacation.endDate}', '${vacation.price}', '${vacation.pictureName}')`
+    const result: OkPacket = await dal.execute(sql);
+    vacation.vacationId = result.insertId
+    return vacation;
+}
+
+async function editVacation(vacation:VacationModel): Promise<VacationModel>{
+    const errors = vacation.validatePut();
+    if (errors) throw new ValidationError(errors);
+    const sql = `UPDATE vacation_table SET
+    destination ='${vacation.destination}',
+    description ='${vacation.description}',
+    startDate ='${vacation.startDate}',
+    endDate ='${vacation.endDate}',
+    price ='${vacation.price}',
+    pictureName ='${vacation.pictureName}'
+    WHERE vacationId = '${vacation.vacationId}'`
+
+    const result: OkPacket = await dal.execute(sql);
+
+    if (result.affectedRows === 0) throw new ResourceNotFoundError(vacation.vacationId);
+
+    return vacation;
+}
+
+
+async function deleteVacation(id: number): Promise<void> {
+
+    // Create query: 
+    const sql = `DELETE FROM vacation_table WHERE vacationId = ${id}`;
+
+    // Execute: 
+    const result: OkPacket = await dal.execute(sql);
+
+    // If product not found:
+    if (result.affectedRows === 0) throw new ResourceNotFoundError(id);
+
+
+}
+
+
+async function addFollow(follower:FollowerModel): Promise<FollowerModel> {
+    const sql = `INSERT INTO followers_table VALUES(${follower.userId},${follower.vacationId})`;
+    const result: OkPacket = await dal.execute(sql);
+    return follower;
+}
+
+
+async function deleteFollow(userId:number, vacationId:number): Promise<void> {
+    const sql = `DELETE FROM followers_table WHERE userId=${userId} AND vacationId=${vacationId}`;
+    const result: OkPacket = await dal.execute(sql);
+}
+
+async function getVacationsReport(): Promise<VacationModel[]> {
+    const sql = `
+        SELECT DISTINCT
+            V.destination,
+            COUNT(F.userId) AS followersCount
+        FROM vacation_table as V LEFT JOIN followers_table as F
+        ON V.vacationId = F.vacationId
+        GROUP BY destination
+        ORDER BY followersCount DESC
+        `;
+    
+    const vacations = await dal.execute(sql);
+
+    return vacations;
+}
+
 export default {
     getAllVacations,
     getAllUsers,
-    getVacations
+    getVacations,
+    getSingleVacation,
+    getVacationsReport,
+    addVacation,
+    editVacation,
+    deleteVacation,
+    addFollow,
+    deleteFollow
 };
 
