6d243ac2f887124087916ef3c300c30fbac8b1ea
Itay <itay3742@gmail.com>
2023-05-06 20:26:57 +0300

    Update Routing.tsx

--- Frontend/src/Components/LayoutArea/Routing/Routing.tsx
+++ Frontend/src/Components/LayoutArea/Routing/Routing.tsx
@@ -3,6 +3,10 @@ import Insert from "../../DataArea/Insert/Insert";
 import List from "../../DataArea/List/List";
 import Home from "../../HomeArea/Home/Home";
 import PageNotFound from "../PageNotFound/PageNotFound";
+import Login from "../../AuthArea/Login/Login";
+import Register from "../../AuthArea/Register/Register";
+import Vacations from "../../DataArea/vacations/vacations";
+import VacationsReport from "../../DataArea/VacationsReport/VacationsReport";
 
 function Routing(): JSX.Element {
     return (
@@ -10,10 +14,14 @@ function Routing(): JSX.Element {
             <Route path="/home" element={<Home />} />
             <Route path="/list" element={<List />} />
             <Route path="/insert" element={<Insert />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/vacations" element={<Vacations />} />
+            <Route path="/vacationsReport" element={<VacationsReport />} />
             <Route path="/" element={<Navigate to="/home" />} />
             <Route path="*" element={<PageNotFound />} />
         </Routes>
     );
 }
-
+// <NavLink to="/vacationsReport">Vacations report</NavLink>
 export default Routing;
