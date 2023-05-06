483db2f2b6b9d11f0e98f848e050f66f2fdf5064
Itay <itay3742@gmail.com>
2023-05-06 20:26:40 +0300

    Update VacationModel.ts

--- Frontend/src/Models/VacationModel.ts
+++ Frontend/src/Models/VacationModel.ts
@@ -7,16 +7,8 @@ class VacationModel {
     public endDate: Date;
     public price: number;
     public pictureName: string;
-
-    public constructor(vacation: VacationModel) {
-        this.vacationId = vacation.vacationId;
-        this.destination = vacation.destination;
-        this.description = vacation.description;
-        this.startDate = vacation.startDate;
-        this.endDate = vacation.endDate;
-        this.price = vacation.price;
-        this.pictureName = vacation.pictureName;
-    }
+    public followersCount?:number;
+    public isFollowing?:number;
 
 }
 
