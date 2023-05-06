94568faf161736adccddf8bdbf97aef8520d4535
Itay <itay3742@gmail.com>
2023-05-06 20:26:28 +0300

    Create Login.tsx

---
+++ Frontend/src/Components/AuthArea/Login/Login.tsx
@@ -0,0 +1,46 @@
+import { useForm } from "react-hook-form";
+import { NavLink, useNavigate } from "react-router-dom";
+import CredentialsModel from "../../../Models/CredentialsModel";
+import authService from "../../../Services/AuthService";
+import notifyService from "../../../Services/NotifyService";
+import "./Login.css";
+
+function Login(): JSX.Element {
+
+    const { register, handleSubmit } = useForm<CredentialsModel>();
+    const navigate = useNavigate();
+
+    async function send(credentials: CredentialsModel) {
+        try {
+            await authService.login(credentials);
+            notifyService.success("Welcome Back!")
+            navigate("/home");
+        }
+        catch (err: any) {
+            notifyService.error(err);
+        }
+    }
+
+    return (
+        <div className="Login Box">
+
+            <h2>Login</h2>
+
+            <form onSubmit={handleSubmit(send)}>
+
+                <label>email:</label>
+                <input type="text" {...register("email")} />
+
+                <label>Password:</label>
+                <input type="password" {...register("password")} />
+
+                <button>Login</button>
+
+            </form>
+        <p>havent signed in yet?</p>
+        <NavLink to="/Register">Register</NavLink>
+        </div>
+    );
+}
+
+export default Login;
