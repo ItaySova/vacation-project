13cf522af48661fdbdf830702fcd49c31f7777c5
Itay <itay3742@gmail.com>
2023-05-07 08:04:47 +0300

    Update vacations.tsx

--- Frontend/src/Components/DataArea/vacations/vacations.tsx
+++ Frontend/src/Components/DataArea/vacations/vacations.tsx
@@ -8,7 +8,7 @@ import VacationCard from "../VacationCard/VacationCard";
 function Vacations(): JSX.Element {
 
     const [vacations, setVacations] = useState<VacationModel[]>([]);
-
+    
     // Get all products once:
     useEffect(() => {
         dataService.getAllVacations()
@@ -17,10 +17,28 @@ function Vacations(): JSX.Element {
                 setVacations(responseVacations)})
             .catch(err => notifyService.error(err));
     }, []);
-    
+
+    // checkbox A is the checkbox for showing only followed vacations
+    // checkbox B is the checkbox for showing only vacations that didn't start yet
+    // checkbox C is the checkbox for showing only active vacations
+    async function handleCheckboxA(){}
+    async function handleCheckboxB(){}
+    async function handleCheckboxC(){}
     return (
         <div className="vacations">
 			<h1>vacation list</h1>
+            <br />
+            <div id="checkBoxSection">
+            <label>show only followed vacations:</label>
+            <input type="checkbox" name="checkboxA" id="checkboxA" />
+            <hr />
+            <label>show only vacations that didn't start yet:</label>
+            <input type="checkbox" name="checkboxB" id="checkboxB" />
+            <hr />
+            <label>show only active vacations:</label>
+            <input type="checkbox" name="checkboxC" id="checkboxC" />
+            </div>
+            <br />
             {vacations.map(p => <VacationCard key={p.vacationId} vacation={p} />)}
         </div>
     );
