ebdc6246000010c95b66e8085c65659532f9352d
Itay <itay3742@gmail.com>
2023-05-13 00:01:15 +0300

    Update VacationsReport.tsx

--- Frontend/src/Components/DataArea/VacationsReport/VacationsReport.tsx
+++ Frontend/src/Components/DataArea/VacationsReport/VacationsReport.tsx
@@ -3,6 +3,8 @@ import VacationModel from "../../../Models/VacationModel";
 import dataService from "../../../Services/DataService";
 import notifyService from "../../../Services/NotifyService";
 import "./VacationsReport.css";
+import ReportChart from "../ReportChart/ReportChart";
+import { CSVLink } from "react-csv";
 
 function VacationsReport(): JSX.Element {
     const [vacations, setVacations] = useState<VacationModel[]>([]);
@@ -10,29 +12,39 @@ function VacationsReport(): JSX.Element {
     // Get all products once:
     useEffect(() => {
         dataService.getAllVacations()
-            .then(responseVacations => {
-                console.log(responseVacations)
-                setVacations(responseVacations)})
-            .catch(err => notifyService.error(err));
+        .then(response => {
+            console.log(response.vacations)
+            setVacations(response.vacations)
+        })
+        .catch(err => notifyService.error(err));
     }, []);
-    
+
+    const headers = [
+        { label: "Destination", key: "Destination" },
+        { label: "Followers", key: "Followers" }
+      ];
+    const data:any[] = []
+    vacations.map(v => {
+        data.push({
+            Destination: v.destination,
+            Followers: v.followersCount
+        })
+    })
+
+    const csvReport = {
+        data: data,
+        headers: headers,
+        filename: 'Report.csv'
+      };
+
     return (
         <div className="VacationsReport">
             <p>vacations report</p>
-            <table>
-                <thead>
-                    <tr>
-                        <th>destentation</th>
-                        <th>followers</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {vacations.map(vacations => <tr key={vacations.vacationId}>
-                        <td>{vacations.destination}</td>
-                        <td>{vacations.followersCount}</td>
-                    </tr>)}
-                </tbody>
-            </table>
+            <br />
+            <ReportChart key={"chart"} vacations={vacations} />
+            <br />
+            <button><CSVLink {...csvReport}>Export to CSV</CSVLink></button>
+            
         </div>
     );
 }
