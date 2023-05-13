5f9bee7ec4ab12d360e88be67143d9cedf67eb63
Itay <itay3742@gmail.com>
2023-05-13 18:51:27 +0300

    Update VacationCardAdmin.tsx

--- Frontend/src/Components/DataArea/VacationCardAdmin/VacationCardAdmin.tsx
+++ Frontend/src/Components/DataArea/VacationCardAdmin/VacationCardAdmin.tsx
@@ -1,11 +1,65 @@
+import { NavLink, useNavigate, useParams } from "react-router-dom";
+import VacationModel from "../../../Models/VacationModel";
 import "./VacationCardAdmin.css";
+import dataService from "../../../Services/DataService";
+import notifyService from "../../../Services/NotifyService";
+import { useEffect } from "react";
 
-function VacationCardAdmin(): JSX.Element {
+interface VacationCardProps {
+    vacation: VacationModel
+}
+
+
+function VacationCardAdmin(props: VacationCardProps): JSX.Element {
+    const startDate = new Date(props.vacation.startDate) // add to checks
+    const endtDate = new Date(props.vacation.endDate)
+    // Get object containing all route params:
+
+    // Navigate hook:
+    const navigate = useNavigate();
+        
+    async function deleteMe() {
+        try {
+            const ok = window.confirm("Are you sure?");
+            if(!ok) return;
+            await dataService.deleteVacation(props.vacation.vacationId);
+            notifyService.success("Product has been deleted");
+            navigate("/list");
+        }
+        catch(err: any) {
+            notifyService.error(err);
+        }
+    }
+
     return (
         <div className="VacationCardAdmin">
-			
+			    <div>
+                vacations id: {props.vacation.vacationId}
+                <br />
+                dest: {props.vacation.destination}
+                <br />
+                Price: {props.vacation.price}
+                <br />
+                description: {props.vacation.description}
+                <br />
+                dates: {startDate.toDateString()} - {endtDate.toDateString()}
+                <br />
+                picture name: {props.vacation.pictureName}
+                <br />
+                followers: {props.vacation.followersCount}
+                <br />
+                follows: {props.vacation.isFollowing}
+                <br />
+                <button>
+                    <NavLink to={"/vacations/edit/" + props.vacation.vacationId}>Edit</NavLink>
+                </button>
+                <span> | </span>
+                <button>
+                    <NavLink to="#" onClick={deleteMe}>Delete</NavLink>
+                </button>
+            </div>
         </div>
     );
 }
-
+// "/vacations/edit/:vacationId"
 export default VacationCardAdmin;
