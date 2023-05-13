7d652d95b3e328e108e30d9c7d8a3667a0fe6dfc
Itay <itay3742@gmail.com>
2023-05-13 08:20:54 +0300

    Create VacationCardAdmin.tsx

---
+++ Frontend/src/Components/DataArea/VacationCardAdmin/VacationCardAdmin.tsx
@@ -0,0 +1,11 @@
+import "./VacationCardAdmin.css";
+
+function VacationCardAdmin(): JSX.Element {
+    return (
+        <div className="VacationCardAdmin">
+			
+        </div>
+    );
+}
+
+export default VacationCardAdmin;
