3786ec24c8b0effd23afff4a9af4485af8720cbb
Itay <itay3742@gmail.com>
2023-05-16 14:13:47 +0300

    Update Home.tsx

--- Frontend/src/Components/HomeArea/Home/Home.tsx
+++ Frontend/src/Components/HomeArea/Home/Home.tsx
@@ -1,9 +1,29 @@
+import { useEffect } from "react";
+import { authStore } from "../../../Redux/AuthState";
 import "./Home.css";
+import RoleModel from "../../../Models/RoleModel";
+import { useNavigate } from "react-router-dom";
 
 function Home(): JSX.Element {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const user = authStore.getState().user;
+        if (!user) {
+            navigate("/login");
+        }
+        if (user.roleId === RoleModel.User){
+            navigate("/vacations")
+        }
+        if (user.roleId === RoleModel.Admin){
+            navigate("/list")
+        }
+
+    }, [])
+
     return (
         <div className="Home">
-			<h2>Home Page</h2>
+			<h2>Home Page -routing to the vacations</h2>
         </div>
     );
 }
