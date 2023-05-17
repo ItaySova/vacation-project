652143ec9b5bdc793ee8e19d2481b5a9c7785783
Itay <itay3742@gmail.com>
2023-05-17 16:53:23 +0300

    Update List.tsx

--- Frontend/src/Components/DataArea/List/List.tsx
+++ Frontend/src/Components/DataArea/List/List.tsx
@@ -7,45 +7,61 @@ import authService from "../../../Services/AuthService";
 import dataService from "../../../Services/DataService";
 import notifyService from "../../../Services/NotifyService";
 import VacationCardAdmin from "../VacationCardAdmin/VacationCardAdmin";
+import { vacationsStore } from "../../../Redux/VacationsState";
 
 function List(): JSX.Element {
     const [vacations, setVacations] = useState<VacationModel[]>([]);
+    const [count, setCount] = useState<number>(0);
     const [pageState, setPageState] = useState<number>(1);
     const [pagesState, setPagesState] = useState<number>(1);
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
     useEffect(() => {
         const user = authStore.getState().user;
         const token = authStore.getState().token
 
-        if (!user || !token) {
+        if (!user) {
             navigate("/login");
+            // return;
         }
+
+        // Take number of products length into count: 
+        setCount(vacationsStore.getState().vacations.length);
+
+        // Subscribe for changes (any dispatch will invoke the callback): 
+        const unsubscribe = vacationsStore.subscribe(() => {
+
+            // Take number of products length into count: 
+            setCount(vacationsStore.getState().vacations.length);
+        });
+
+        // Stop listening when component destroyed:
+        return () => unsubscribe();
     }, [])
 
     // Get all products once:
     useEffect(() => {
-        dataService.getAllVacations({ page: pageState}) // ADD HERE
+        dataService.getAllVacations({ page: pageState })
             .then(response => {
-                console.log(response.vacations)
                 setVacations(response.vacations)
                 setPagesState(response.numOfPages)
             })
             .catch(err => {
-                // if (err.response.data === 'Invalid token'){
-                //     authService.logout();
-                //     navigate("/login");
-                // }
-                return notifyService.error(err)});
-    }, [pageState]);
+                if (err.response.data === 'Invalid token'){
+                    authService.logout();
+                    navigate("/login");
+                }
+                return notifyService.error(err)
+            });
+    }, [pageState, count]);
     return (
         <div className="List">
-			<h2>vacations Page</h2>
+            <h2>vacations Page</h2>
             <hr />
             <div>
                 {vacations.map(p => <VacationCardAdmin key={p.vacationId} vacation={p} />)}
             </div>
             {Array.from(Array(pagesState)).map((_, index: number) => {
-                return <button type="button" className="vacationsPage" onClick={() => setPageState(index + 1)}>{index + 1}</button>;
+                return <button type="button" key={index + 1} className="vacationsPage" onClick={() => setPageState(index + 1)}>{index + 1}</button>;
             })}
         </div>
     );
