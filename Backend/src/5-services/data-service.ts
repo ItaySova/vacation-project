3105f618199b1842f952c2c46ac338088969da60
Itay <itay3742@gmail.com>
2023-05-17 16:52:52 +0300

    Update data-service.ts

--- Backend/src/5-services/data-service.ts
+++ Backend/src/5-services/data-service.ts
@@ -1,8 +1,10 @@
 import UserModel from "../2-models/user-model";
 import { ResourceNotFoundError, ValidationError } from "../2-models/client-errors";
 import VacationModel from "../2-models/vacation-model";
+import imageHandler from "../4-utils/image-handler";
 import dal from "../4-utils/dal";
 import { OkPacket } from "mysql";
+import appConfig from "../4-utils/app-config";
 
 async function getAllVacations(): Promise<VacationModel[]> {
     const sql = "SELECT * FROM vacation_table";
@@ -20,20 +22,39 @@ async function getAllUsers(): Promise<UserModel[]> {
 
 
 // will be used later with authentication
-async function getVacations(userId: number, options?: { page?: number, showFollowed?: boolean, showFuture?:boolean, showActive?:boolean }): Promise<{ vacations: VacationModel[], numOfPages: number }> {
+async function getVacations(userId: number, options?: { page?: number, showFollowed?: boolean, showFuture?: boolean, showActive?: boolean }): Promise<{ vacations: VacationModel[], numOfPages: number }> {
     let condition = ``
-    if (options.showFollowed){
+    if (options.showFollowed) {
         condition += `HAVING isFollowing=1`
     }
-    if (options.showFuture){
+    if (options.showFuture) {
         condition += `HAVING startDate > CURRENT_DATE()`
     }
-    if (options.showActive){
+    if (options.showActive) {
         condition += `HAVING startDate <= CURRENT_DATE() AND endDate >= CURRENT_DATE()`
     }
+    // remove at final edit
     let sql = `
         SELECT DISTINCT
             V.*,
+            EXISTS(SELECT * FROM followers_table WHERE vacationId = F.vacationId AND userId = ?) AS isFollowing,
+            COUNT(F.userId) AS followersCount
+        FROM vacation_table as V LEFT JOIN followers_table as F
+        ON V.vacationId = F.vacationId
+        GROUP BY vacationId
+        ${condition}
+        ORDER BY startDate
+        `;
+
+    let sql2 = `
+        SELECT DISTINCT
+            V.vacationId,
+            V.destination,
+            V.description,
+            V.startDate,
+            V.endDate,
+            V.price,
+            CONCAT('${appConfig.imagesUrl}', V.pictureName) AS pictureName,
             EXISTS(SELECT * FROM followers_table WHERE vacationId = F.vacationId AND userId = ?) AS isFollowing,
             COUNT(F.userId) AS followersCount
         FROM vacation_table as V LEFT JOIN followers_table as F
@@ -49,12 +70,16 @@ async function getVacations(userId: number, options?: { page?: number, showFollo
             LIMIT 9
             OFFSET ${(options.page - 1) * 9}
         `;
+        sql2 += `
+            LIMIT 9
+            OFFSET ${(options.page - 1) * 9}
+        `;
     }
-    let vacations = await dal.execute(sql, [userId]);
+    let vacations = await dal.execute(sql2, [userId]);
 
-    const numOfPagesQuery =`SELECT COUNT(vacationId) AS numOfVacations FROM vacation_table`;
+    const numOfPagesQuery = `SELECT COUNT(vacationId) AS numOfVacations FROM vacation_table`;
     const numOfPagesRes = await dal.execute(numOfPagesQuery)
-    const numOfPages = Math.ceil(numOfPagesRes[0].numOfVacations / 10)
+    const numOfPages = Math.ceil(numOfPagesRes[0].numOfVacations / 9)
 
 
     return {
@@ -65,7 +90,16 @@ async function getVacations(userId: number, options?: { page?: number, showFollo
 
 async function getSingleVacation(id: number): Promise<VacationModel> {
     const sql = `SELECT * FROM vacation_table WHERE vacationId = ${id}`
-    const vacations = await dal.execute(sql);
+    const sql2 = `SELECT vacationId,
+    destination,
+    description,
+    startDate,
+    endDate,
+    price,
+    CONCAT('${appConfig.imagesUrl}', pictureName) AS pictureName
+    FROM vacation_table WHERE vacationId = ${id}`
+    //appConfig.imagesUrl
+    const vacations = await dal.execute(sql2);
     const vacation = vacations[0];
     if (!vacation) throw new ResourceNotFoundError(id);
     return vacation;
@@ -77,17 +111,43 @@ async function addVacation(vacation: VacationModel): Promise<VacationModel> {
     const errors = vacation.validatePost();
     if (errors) throw new ValidationError(errors);
 
+    let imageName = null;
+
+    // If we have image:
+    if (vacation.image) {
+
+        // Save image: 
+        imageName = await imageHandler.saveImage(vacation.image);
+
+        // Set back image url: 
+        vacation.pictureName = appConfig.imagesUrl + imageName;
+    }
+
     const sql = `INSERT INTO vacation_table(destination,description,startDate,endDate,price,pictureName)
     VALUES ('${vacation.destination}', '${vacation.description}',
-            '${vacation.startDate}', '${vacation.endDate}', '${vacation.price}', '${vacation.pictureName}')`
+            '${vacation.startDate}', '${vacation.endDate}', '${vacation.price}', '${imageName}')`
     const result: OkPacket = await dal.execute(sql);
     vacation.vacationId = result.insertId
+
+    delete vacation.image;
+
     return vacation;
 }
 
 async function editVacation(vacation: VacationModel): Promise<VacationModel> {
-    // console.log("from backend service:")
-    // console.log(vacation)
+
+    // Take original image name: 
+    let imageName = await getVacationPictureName(vacation.vacationId);
+
+    // If we have an image to update:
+    if (vacation.image) {
+        // Update image:
+        imageName = await imageHandler.updateImage(vacation.image, imageName);
+    }
+
+    // Set back image url: 
+    vacation.pictureName = appConfig.imagesUrl + imageName;
+
     const errors = vacation.validatePut();
     if (errors) throw new ValidationError(errors);
     const sql = `UPDATE vacation_table SET
@@ -96,12 +156,15 @@ async function editVacation(vacation: VacationModel): Promise<VacationModel> {
     startDate ='${vacation.startDate}',
     endDate ='${vacation.endDate}',
     price ='${vacation.price}',
-    pictureName ='${vacation.pictureName}'
+    pictureName ='${imageName}'
     WHERE vacationId = '${vacation.vacationId}'`
 
     const result: OkPacket = await dal.execute(sql);
 
     if (result.affectedRows === 0) throw new ResourceNotFoundError(vacation.vacationId);
+
+    // Remove image file from returned product:
+    delete vacation.image;
 
     return vacation;
 }
@@ -109,6 +172,8 @@ async function editVacation(vacation: VacationModel): Promise<VacationModel> {
 
 async function deleteVacation(id: number): Promise<void> {
 
+    // Take original image name: 
+    let imageName = await getVacationPictureName(id);
     // Create query: 
     const sql = `DELETE FROM vacation_table WHERE vacationId = ${id}`;
 
@@ -118,19 +183,31 @@ async function deleteVacation(id: number): Promise<void> {
     // If product not found:
     if (result.affectedRows === 0) throw new ResourceNotFoundError(id);
 
+    // Delete image from disk:
+    await imageHandler.deleteImage(imageName);
 
 }
 
 
-async function addFollow(userId: number, vacationId: number): Promise<number> {
+async function addFollow(userId: number, vacationId: number): Promise<number[]> {
     const sql = `INSERT INTO followers_table VALUES(${userId},${vacationId})`;
     const result: OkPacket = await dal.execute(sql);
-    return result.affectedRows;
+    // return result.affectedRows;
+    return [
+        userId,
+        vacationId
+    ];
 }
 
 
 async function deleteFollow(userId: number, vacationId: number): Promise<void> {
     const sql = `DELETE FROM followers_table WHERE userId=${userId} AND vacationId=${vacationId}`;
+    const result: OkPacket = await dal.execute(sql);
+}
+
+// in case of deleting a vacation - remove all its likes from the table
+async function deleteVacationsFollows(vacationId: number): Promise<void> {
+    const sql = `DELETE FROM followers_table WHERE vacationId=${vacationId}`;
     const result: OkPacket = await dal.execute(sql);
 }
 
@@ -150,6 +227,28 @@ async function getVacationsReport(): Promise<VacationModel[]> {
     return vacations;
 }
 
+// Get product image name from db:
+async function getVacationPictureName(id: number): Promise<string> {
+
+    // Create query: 
+    const sql = `SELECT pictureName FROM vacation_table WHERE vacationId = ${id}`;
+
+    // Get products: 
+    const vacations = await dal.execute(sql);
+
+    // Extract first product: 
+    const vacation = vacations[0];
+
+    // If id not found: 
+    if (!vacation) return null;
+
+    // Get image name: 
+    const imageName = vacation.pictureName;
+
+    // Return: 
+    return imageName;
+}
+
 export default {
     getAllVacations,
     getAllUsers,
@@ -160,6 +259,7 @@ export default {
     editVacation,
     deleteVacation,
     addFollow,
-    deleteFollow
+    deleteFollow,
+    deleteVacationsFollows
 };
 
