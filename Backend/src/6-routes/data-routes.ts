8a31c3e2c5ec4cd1f3df46089f71d66483ef9666
Itay <itay3742@gmail.com>
2023-05-17 16:53:27 +0300

    Update data-routes.ts

--- Backend/src/6-routes/data-routes.ts
+++ Backend/src/6-routes/data-routes.ts
@@ -5,6 +5,7 @@ import FollowerModel from "../2-models/follower-model";
 import verifyLoggedIn from "../3-middleware/verify-logged-in";
 import cyber from "../4-utils/cyber";
 import verifyAdmin from "../3-middleware/verify-admin";
+import imageHandler from "../4-utils/image-handler";
 
 // todo - update the permissions
 
@@ -63,6 +64,7 @@ router.get("/vacations-report", verifyAdmin, async (request: Request, response: 
 
 router.post("/vacations", verifyAdmin, async (request: Request, response: Response, next: NextFunction) => {
     try {
+        request.body.image = request.files?.image;
         const vacation = new VacationModel(request.body)
         const addedVacation = await dataService.addVacation(vacation)
         response.status(201).json(addedVacation)
@@ -75,9 +77,8 @@ router.post("/vacations", verifyAdmin, async (request: Request, response: Respon
 // edit
 router.put("/vacations/:id([0-9]+)", verifyAdmin, async (request: Request, response: Response, next: NextFunction) => {
     try {
+        request.body.image = request.files?.image;
         request.body.vacationId = +request.params.id
-        // console.log("from route")
-        // console.log(request.headers.)
         const vacation = new VacationModel(request.body)
         const updatedVacation = await dataService.editVacation(vacation)
         response.json(updatedVacation)
@@ -91,6 +92,7 @@ router.delete("/vacations/:id([0-9]+)", verifyAdmin, async (request: Request, re
     try {
         const id = +request.params.id;
         await dataService.deleteVacation(id)
+        // await dataService.deleteVacationsFollows(id)
         response.sendStatus(204);
     }
     catch (err: any) {
@@ -115,10 +117,21 @@ router.post("/follower/:vacationId([0-9]+)", verifyLoggedIn, async (request: Req
 router.delete("/follower/:vacationId([0-9]+)", verifyLoggedIn, async (request: Request, response: Response, next: NextFunction) => {
     try {
         const user = await cyber.decodeUser(request)
-        // const userId = +request.params.userId
         const vacationId = +request.params.vacationId
         await dataService.deleteFollow(user.userId, vacationId)
-        response.sendStatus(204);
+        response.sendStatus(204)
+    }
+    catch (err: any) {
+        next(err);
+    }
+});
+
+// GET the image
+router.get("/vacations/images/:imageName", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const imageName = request.params.imageName;
+        const imagePath = imageHandler.getImagePath(imageName);
+        response.sendFile(imagePath);
     }
     catch (err: any) {
         next(err);
