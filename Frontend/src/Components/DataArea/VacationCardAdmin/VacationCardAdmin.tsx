fb5145a8b8dbd17a78768d871b2cfaa515ace932
Itay <itay3742@gmail.com>
2023-05-18 08:50:32 +0300

    Update VacationCardAdmin.tsx

--- Frontend/src/Components/DataArea/VacationCardAdmin/VacationCardAdmin.tsx
+++ Frontend/src/Components/DataArea/VacationCardAdmin/VacationCardAdmin.tsx
@@ -11,39 +11,39 @@ interface VacationCardProps {
 
 
 function VacationCardAdmin(props: VacationCardProps): JSX.Element {
-    const startDate = new Date(props.vacation.startDate) // add to checks
+    const startDate = new Date(props.vacation.startDate)
     const endtDate = new Date(props.vacation.endDate)
     // Get object containing all route params:
 
     // Navigate hook:
     const navigate = useNavigate();
-        
+
     async function deleteMe() {
         try {
             const ok = window.confirm("Are you sure?");
-            if(!ok) return;
+            if (!ok) return;
             await dataService.deleteVacation(props.vacation.vacationId);
             notifyService.success("vacation has been deleted");
             navigate("/list");
         }
-        catch(err: any) {
+        catch (err: any) {
             notifyService.error(err);
         }
     }
 
     return (
         <div className="VacationCardAdmin">
-			    <div>
-                <img src={props.vacation.pictureName} />
-                <br />
-                dates: {startDate.toDateString()} - {endtDate.toDateString()}
-                <br />
-                dest: {props.vacation.destination}
-                <br />
-                description: {props.vacation.description}
-                <br />
-                Price: {props.vacation.price}                
-                <br />
+            <div>
+                <div className="card_image">
+                    <img src={props.vacation.pictureName} />
+                </div>
+                <div className="date">
+                    dates: {startDate.toDateString()} - {endtDate.toDateString()}
+                </div>
+                <div className="content">
+                    <p>dest: {props.vacation.destination}</p>
+                    <p>description: {props.vacation.description}</p>
+                </div>
                 <button>
                     <NavLink to={"/vacations/edit/" + props.vacation.vacationId}>Edit</NavLink>
                 </button>
@@ -51,6 +51,9 @@ function VacationCardAdmin(props: VacationCardProps): JSX.Element {
                 <button>
                     <NavLink to="#" onClick={deleteMe}>Delete</NavLink>
                 </button>
+                <div className="priceTag">
+                    Price: {props.vacation.price}
+                </div>
             </div>
         </div>
     );
