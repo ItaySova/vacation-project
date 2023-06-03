c4db3266751cf1faf85631ccc14f71b0d4167bce
Itay <itay3742@gmail.com>
2023-06-03 19:28:28 +0300

    Update VacationCardAdmin.tsx

--- Frontend/src/Components/DataArea/VacationCardAdmin/VacationCardAdmin.tsx
+++ Frontend/src/Components/DataArea/VacationCardAdmin/VacationCardAdmin.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useNavigate, useParams } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import VacationModel from "../../../Models/VacationModel";
 import "./VacationCardAdmin.css";
 import dataService from "../../../Services/DataService";
