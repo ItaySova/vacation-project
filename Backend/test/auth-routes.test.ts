6354973473276cf3b698eaab7cfa83f9e231a912
Itay <itay3742@gmail.com>
2023-07-28 17:52:23 +0300

    Create auth-routes.test.ts

---
+++ Backend/test/auth-routes.test.ts
@@ -0,0 +1,20 @@
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import app from "../src/app";
+import supertest from "supertest";
+import RoleModel from "../src/2-models/role-model";
+
+
+describe("testing auth routes", ()=>{
+    it("should register existing admin", async ()=>{
+        const response = await supertest(app.server).post("/api/register").send({ firstName: 'admin', lastName: 'main', email: 'admin@gmail.com', password: 'hardpass', roleId: RoleModel.Admin})
+        expect(response).to.haveOwnProperty("status", 400);
+    });
+
+    it("should sign in", async ()=>{
+        const response = await supertest(app.server).post("/api/login").send({ email: 'admin@gmail.com', password: 'hardpass' })
+        const token = response.body;
+        expect(token).to.be.not.empty;
+    });
+});
+
