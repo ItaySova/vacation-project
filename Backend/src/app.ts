9448b675c21cca76e86ea4594e769ccb1b81ff8f
Itay <itay3742@gmail.com>
2023-07-29 18:38:22 +0300

    Update app.ts

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import expressFileUpload from "express-fileupload";
 import dataRoutes from "./6-routes/data-routes";
+import usersRoutes from "./6-routes/users-routes"
 import authRoute from "./6-routes/auth-routes";
 import routeNotFound from "./3-middleware/route-not-found";
 import catchAll from "./3-middleware/catch-all";
@@ -13,6 +14,7 @@ server.use(cors({ origin: "http://localhost:3000" }));
 server.use(express.json());
 server.use(expressFileUpload());
 server.use("/api", dataRoutes);
+server.use("/api", usersRoutes);
 server.use("/api", authRoute);
 server.use(routeNotFound);
 server.use(catchAll);
