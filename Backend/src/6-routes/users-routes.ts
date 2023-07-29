0805b88539a098ea8f3f908f6ec327e80f504872
Itay <itay3742@gmail.com>
2023-07-29 18:45:02 +0300

    Update users-routes.ts

--- Backend/src/6-routes/users-routes.ts
+++ Backend/src/6-routes/users-routes.ts
@@ -13,4 +13,15 @@ router.get("/users", async (request: Request, response: Response, next: NextFunc
     }
 });
 
+router.get("/users/:userId",  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const userId = +request.params.userId
+        const users = await usersService.getOneUser(userId)
+        response.json(users)
+    }
+    catch (err: any) {
+        next(err);
+    }
+})
+
 export default router;
