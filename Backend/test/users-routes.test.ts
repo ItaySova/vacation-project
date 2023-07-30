ecae62049d27213e98665c0d9888a994a7c6cc13
Itay <itay3742@gmail.com>
2023-07-30 22:32:25 +0300

    Update users-routes.test.ts

--- Backend/test/users-routes.test.ts
+++ Backend/test/users-routes.test.ts
@@ -2,3 +2,23 @@ import { describe, it } from "mocha";
 import { expect } from "chai";
 import app from "../src/app";
 import supertest from "supertest";
+
+
+describe("testing users routes", ()=>{
+    it("should register existing admin", async ()=>{
+        // todo - add test for get
+    });
+
+    it("should sign in", async ()=>{
+        // todo -add test for get one user
+    });
+
+    it("should sign in", async ()=>{
+        // todo - add test for edit user
+    });
+
+    it("should sign in", async ()=>{
+        // add test to remove user
+    });
+});
+
