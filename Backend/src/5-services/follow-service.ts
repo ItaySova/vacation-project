81dadfe2eb748694091efa7afb23596cbb75e81a
Itay <itay3742@gmail.com>
2023-07-31 21:02:19 +0300

    Create follow-service.ts

---
+++ Backend/src/5-services/follow-service.ts
@@ -0,0 +1,24 @@
+import dal from "../4-utils/dal";
+import { OkPacket } from "mysql";
+
+
+async function addFollow(userId: number, vacationId: number): Promise<number[]> {
+    const sql = `INSERT INTO followers_table VALUES(${userId},${vacationId})`;
+    const result: OkPacket = await dal.execute(sql);
+    // return result.affectedRows;
+    return [
+        userId,
+        vacationId
+    ];
+}
+
+async function deleteFollow(userId: number, vacationId: number): Promise<void> {
+    const sql = `DELETE FROM followers_table WHERE userId=${userId} AND vacationId=${vacationId}`;
+    const result: OkPacket = await dal.execute(sql);
+}
+
+// in case of deleting a vacation - remove all its likes from the table
+async function deleteVacationsFollows(vacationId: number): Promise<void> {
+    const sql = `DELETE FROM followers_table WHERE vacationId=${vacationId}`;
+    const result: OkPacket = await dal.execute(sql);
+}
