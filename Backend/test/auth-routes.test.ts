8f3fef9d37c0253230e43de7798c4109437e781a
Itay <itay3742@gmail.com>
2023-08-01 22:03:38 +0300

    Update auth-routes.test.ts

--- Backend/test/auth-routes.test.ts
+++ Backend/test/auth-routes.test.ts
@@ -6,6 +6,13 @@ import RoleModel from "../src/2-models/role-model";
 
 
 describe("testing auth routes", ()=>{
+
+    it("should register test user", async ()=>{
+        const response = await supertest(app.server).post("/api/register").send({ firstName: 'test', lastName: 'test', email: 'test@gmail.com', password: 'hardpass', roleId: RoleModel.User})
+        const token = response.body;
+        expect(token).to.be.not.empty;
+    });
+
     it("should register existing admin", async ()=>{
         const response = await supertest(app.server).post("/api/register").send({ firstName: 'admin', lastName: 'main', email: 'admin@gmail.com', password: 'hardpass', roleId: RoleModel.Admin})
         expect(response).to.haveOwnProperty("status", 400);
