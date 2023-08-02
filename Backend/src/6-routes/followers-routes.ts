e737a1078c27de78c0ad6a67e51d911e16ace7da
Itay <itay3742@gmail.com>
2023-08-02 20:54:41 +0300

    Update followers-routes.ts

--- Backend/src/6-routes/followers-routes.ts
+++ Backend/src/6-routes/followers-routes.ts
@@ -4,3 +4,15 @@ import cyber from "../4-utils/cyber";
 import followService from "../5-services/follow-service";
 
 const router = express.Router();
+
+router.post("/follower/:vacationId([0-9]+)", verifyLoggedIn, async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const user = await cyber.decodeUser(request)
+        const vacationId = +request.params.vacationId
+        const addedFollower = await followService.addFollow(user.userId, vacationId)
+        response.status(201).json(addedFollower)
+    }
+    catch (err: any) {
+        next(err);
+    }
+});
