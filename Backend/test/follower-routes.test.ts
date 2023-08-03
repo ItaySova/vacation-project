3240a8d04067c2a85efe1294429ea44d14305351
Itay <itay3742@gmail.com>
2023-08-03 23:11:31 +0300

    Create follower-routes.test.ts

---
+++ Backend/test/follower-routes.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import app from "../src/app";
+import supertest from "supertest";
+
+
+describe("testing followers routes", ()=>{
+
+});
