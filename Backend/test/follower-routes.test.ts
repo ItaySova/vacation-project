7ecde667fa124cecff0a4d61c0d2fde310347f82
Itay <itay3742@gmail.com>
2023-08-03 23:11:58 +0300

    Update follower-routes.test.ts

--- Backend/test/follower-routes.test.ts
+++ Backend/test/follower-routes.test.ts
@@ -5,5 +5,10 @@ import supertest from "supertest";
 
 
 describe("testing followers routes", ()=>{
+    let token = null
 
+    before(async () => {
+        const response = await supertest(app.server).post("/api/login").send({ email: 'admin@gmail.com', password: 'hardpass' })
+        token = response.body;
+      });
 });
