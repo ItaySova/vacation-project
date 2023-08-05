6954e91ea2cfcedb0c2a7f2c7deb4e1e32077baf
Itay <itay3742@gmail.com>
2023-08-05 16:22:59 +0300

    Update catch-all.ts

--- Backend/src/3-middleware/catch-all.ts
+++ Backend/src/3-middleware/catch-all.ts
@@ -1,9 +1,12 @@
 import { NextFunction, Request, Response } from "express";
+import logger from "../4-utils/log-helper";
 
 function catchAll(err: any, request: Request, response: Response, next: NextFunction) {
     
     // Display error: 
     console.log(err);
+
+    logger.error(err.message)
 
     // Find status code: 
     const statusCode = err.status || 500; // Short Circuit
