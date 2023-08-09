84d55ec16fe30727c392e8ea3b2c3935f865e8b7
Itay <itay3742@gmail.com>
2023-08-09 13:03:16 +0300

    Update UsersTable.tsx

--- Frontend/src/Components/DataArea/UsersTable/UsersTable.tsx
+++ Frontend/src/Components/DataArea/UsersTable/UsersTable.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { authStore } from "../../../Redux/AuthState";
 import "./UsersTable.css";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import UserModel from "../../../Models/UserModel";
 import userService from "../../../Services/UsersService";
 import notifyService from "../../../Services/NotifyService";
@@ -10,7 +10,7 @@ function UsersTable(): JSX.Element {
     const navigate = useNavigate();
 
     const [users, setUsers] = useState<UserModel[]>([])
-    
+
     useEffect(() => {
         const user = authStore.getState().user;
         if (!user) {
@@ -18,14 +18,36 @@ function UsersTable(): JSX.Element {
             return;
         }
         userService.getAllUsers()
-        .then(res => setUsers(res))
-        .catch(err => notifyService.error(err))
+            .then(res => setUsers(res))
+            .catch(err => notifyService.error(err))
 
     }, [])
 
     return (
         <div className="UsersTable">
-			<h1>will be users page</h1>
+            <h1>will be users page</h1>
+            <table>
+                <tr>
+                    <th>first name</th>
+                    <th>last name</th>
+                    <th>email</th>
+                    <th>edit</th>
+                    <th>remove</th>
+                </tr>
+                {users.map(user => 
+                    <tr>
+                    <td>{user.firstName}</td>
+                    <td>{user.lastName}</td>
+                    <td>{user.email}</td>
+                    <td><button>
+                            <NavLink to={"/users/edit/" + user.userId}>Edit</NavLink>
+                        </button>
+                        </td>
+                    <td>remove</td>
+                    </tr>
+                )}
+                
+            </table>
         </div>
     );
 }
