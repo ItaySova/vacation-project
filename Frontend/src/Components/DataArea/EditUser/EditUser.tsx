bf5a273cc1d18c43edb42cfabc76dd602daaadb3
Itay <itay3742@gmail.com>
2023-08-09 14:44:19 +0300

    Create EditUser.tsx

---
+++ Frontend/src/Components/DataArea/EditUser/EditUser.tsx
@@ -0,0 +1,15 @@
+import "./EditUser.css";
+
+interface EditUserProps {
+	
+}
+
+function EditUser(props: EditUserProps): JSX.Element {
+    return (
+        <div className="EditUser">
+			edit user form
+        </div>
+    );
+}
+
+export default EditUser;
