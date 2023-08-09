c2332fe36ec6ac5e0c4a0a479c8fc56390d5b792
Itay <itay3742@gmail.com>
2023-08-09 15:59:09 +0300

    Update EditUser.tsx

--- Frontend/src/Components/DataArea/EditUser/EditUser.tsx
+++ Frontend/src/Components/DataArea/EditUser/EditUser.tsx
@@ -2,6 +2,10 @@ import { useNavigate, useParams } from "react-router-dom";
 import "./EditUser.css";
 import { useForm } from "react-hook-form";
 import UserModel from "../../../Models/UserModel";
+import { useEffect, useState } from "react";
+import dataService from "../../../Services/DataService";
+import userService from "../../../Services/UsersService";
+import notifyService from "../../../Services/NotifyService";
 
 
 function EditUser(): JSX.Element {
@@ -9,7 +13,22 @@ function EditUser(): JSX.Element {
     const params = useParams();
     const { register, handleSubmit, setValue } = useForm<UserModel>();
     const navigate = useNavigate();
+    const [user, setUser] = useState<UserModel>()
 
+    useEffect(()=>{
+        const id = +params.userId;
+        userService.getOneUser(id)
+        .then(res => {
+            setUser(res);
+            setValue("userId", res.userId);
+            setValue("firstName", res.firstName);
+            setValue("lastName", res.lastName);
+            setValue("email", res.email);
+            setValue("roleId", res.roleId);
+            setValue("password", res.password);
+        })
+        .catch(err => notifyService.error(err))
+    },[])
     return (
         <div className="EditUser">
 			edit user form
