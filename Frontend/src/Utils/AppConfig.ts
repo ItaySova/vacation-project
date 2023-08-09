6319f08e3825e6cc8560e1fd6bd772638308f263
Itay <itay3742@gmail.com>
2023-08-09 15:03:36 +0300

    Update AppConfig.ts

--- Frontend/src/Utils/AppConfig.ts
+++ Frontend/src/Utils/AppConfig.ts
@@ -4,7 +4,7 @@ class AppConfig {
     public vacationsUrl = "http://localhost:4000/api/vacations/";
     public imagesUrl = "http://localhost:4000/api/vacations/images/";
     public followerUrl = "http://localhost:4000/api/follower/";
-    public userUrl = "http://localhost:4000/api/Users/";
+    public userUrl = "http://localhost:4000/api/users/";
 }
 
 const appConfig = new AppConfig();
