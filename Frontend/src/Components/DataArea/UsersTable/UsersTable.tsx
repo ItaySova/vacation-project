640ac4be684d4e746da5dc5895175e2ba9e9d74e
Itay <itay3742@gmail.com>
2023-08-09 12:33:10 +0300

    Update UsersTable.tsx

--- Frontend/src/Components/DataArea/UsersTable/UsersTable.tsx
+++ Frontend/src/Components/DataArea/UsersTable/UsersTable.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+import { authStore } from "../../../Redux/AuthState";
 import "./UsersTable.css";
+import { useNavigate } from "react-router-dom";
 
 function UsersTable(): JSX.Element {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const user = authStore.getState().user;
+        if (!user) {
+            navigate("/login");
+            return;
+        }
+
+    }, [])
+
     return (
         <div className="UsersTable">
 			will be users page
