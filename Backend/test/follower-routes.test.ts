f7c30830a972bf8644dafc299e8aa3c3f0100bcd
Itay <itay3742@gmail.com>
2023-08-10 12:18:38 +0300

    Update follower-routes.test.ts

--- Backend/test/follower-routes.test.ts
+++ Backend/test/follower-routes.test.ts
@@ -8,12 +8,16 @@ describe("testing followers routes", ()=>{
     let token = null
 
     before(async () => {
-        const response = await supertest(app.server).post("/api/login").send({ email: 'admin@gmail.com', password: 'hardpass' })
+        const response = await supertest(app.server).post("/api/login").send({ email: 'admin@gmail.com', password: 'hardpass' });
         token = response.body;
       });
 
       it("should add a follow to a vacation", async ()=>{
-        // add later
+        const response = await supertest(app.server).post("/api/follower/1").set('Authorization', 'Bearer ' + token);
+        const follow = JSON.parse(response.text);
+        expect(response).to.haveOwnProperty("status", 201);
+        expect(follow).to.haveOwnProperty("userId");
+        expect(follow).to.haveOwnProperty("vacationId");
     });
 
     it("should remove the added follow from last test", async ()=>{
