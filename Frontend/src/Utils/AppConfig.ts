6ab7765f53bc8ca3b75b3175ed1836c04d099ae7
Itay <itay3742@gmail.com>
2023-08-26 17:34:24 +0300

    Update AppConfig.ts

--- Frontend/src/Utils/AppConfig.ts
+++ Frontend/src/Utils/AppConfig.ts
@@ -1,10 +1,10 @@
 class AppConfig {
-    public registerUrl = "http://localhost:4000/api/register/";
-    public loginUrl = "http://localhost:4000/api/login/";
-    public vacationsUrl = "http://localhost:4000/api/vacations/";
-    public imagesUrl = "http://localhost:4000/api/vacations/images/";
-    public followerUrl = "http://localhost:4000/api/follower/";
-    public userUrl = "http://localhost:4000/api/users/";
+    public registerUrl = "https://localhost:4000/api/register/";
+    public loginUrl = "https://localhost:4000/api/login/";
+    public vacationsUrl = "https://localhost:4000/api/vacations/";
+    public imagesUrl = "https://localhost:4000/api/vacations/images/";
+    public followerUrl = "https://localhost:4000/api/follower/";
+    public userUrl = "https://localhost:4000/api/users/";
 }
 
 const appConfig = new AppConfig();
