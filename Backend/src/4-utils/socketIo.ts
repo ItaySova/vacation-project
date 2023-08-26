846dff8f0c3218d7b284b480b7ee8e114e662b81
Itay <itay3742@gmail.com>
2023-08-26 19:08:37 +0300

    Update socketIo.ts

--- Backend/src/4-utils/socketIo.ts
+++ Backend/src/4-utils/socketIo.ts
@@ -2,7 +2,15 @@ import {Server as HTTPSServer} from 'https'
 import {Server as SocketIoServer} from 'socket.io'
 
 function socketIoLogic(httpsServer: HTTPSServer):void{
+    const options = {
+        cors: {
+            origin: "http://localhost:3000"
+        }
+    }
     const mySocketIOServer = new SocketIoServer(httpsServer);
+    mySocketIOServer.sockets.on("connection", ()=>{
+        console.log("connected to socket")
+    })
 }
 
 export default socketIoLogic;
