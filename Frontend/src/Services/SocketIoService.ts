3068f7d6835bdd7ac8c22fa81175ff96e0482ae1
Itay <itay3742@gmail.com>
2023-09-02 19:18:49 +0300

    Update SocketIoService.ts

--- Frontend/src/Services/SocketIoService.ts
+++ Frontend/src/Services/SocketIoService.ts
@@ -1,9 +1,16 @@
-import { Socket } from "socket.io-client";
+import { Socket, io } from "socket.io-client";
 
 
 
 class SocketIoService{
-    private socket: Socket
+    private socket: Socket;
+
+    public connect(gotMassage: Function){
+        this.socket = io("https://localhost:4000");
+        this.socket.on("msg-from-server", (msg:string)=>{
+            gotMassage(msg);
+        })
+    }
 }
 
 const socketService = new SocketIoService();
