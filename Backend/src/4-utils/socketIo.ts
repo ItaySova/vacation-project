c8dfffc76bba0631fa6e7c6beb5262e5b242315b
Itay <itay3742@gmail.com>
2023-09-09 13:44:08 +0300

    Update socketIo.ts

--- Backend/src/4-utils/socketIo.ts
+++ Backend/src/4-utils/socketIo.ts
@@ -7,17 +7,19 @@ function socketIoLogic(httpsServer: HTTPSServer):void{
             origin: "http://localhost:3000"
         }
     }
-    const mySocketIOServer = new SocketIoServer(httpsServer);
+    const mySocketIOServer = new SocketIoServer(httpsServer, options);
     mySocketIOServer.sockets.on("connection", (socket : Socket)=>{
-        console.log("connected to socket")
+        console.log(`connected to socket, number of clients: ${mySocketIOServer.engine.clientsCount}`)
 
         socket.on("msg-from-client", (msg:string)=>{
             socket.emit("msg-from-server", msg) // echo the massage
+            console.log(`massage emitted: ${msg}`)
+            // mySocketIOServer.sockets.emit("msg-from-server", msg)
         })
 
         socket.on("disconnect", ()=>{
-            socket.emit("msg-from-server", "client has left the chat")
-            console.log("client has disconnected")
+            mySocketIOServer.sockets.emit("msg-from-server", "client has left the chat")
+            console.log(`client has disconnected, remaining clients: ${mySocketIOServer.engine.clientsCount}`)
         })
     })
 }
