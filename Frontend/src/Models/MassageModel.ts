f72dbffc4384cbc8b332d83713488af98d37d999
Itay <itay3742@gmail.com>
2023-09-13 11:58:19 +0300

    Create MassageModel.ts

---
+++ Frontend/src/Models/MassageModel.ts
@@ -0,0 +1,13 @@
+class MassageModel {
+
+    public name:string;
+    public massage: string;
+
+    public constructor(name: string, massage:string) {
+        this.name = name;
+        this.massage = massage;
+    }
+
+}
+
+export default MassageModel;
