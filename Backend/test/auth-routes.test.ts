41206f45e54f4d4964633c3b33c0c56b4812bfd1
Itay <itay3742@gmail.com>
2023-09-18 18:25:12 +0300

    Update auth-routes.test.ts

--- Backend/test/auth-routes.test.ts
+++ Backend/test/auth-routes.test.ts
@@ -8,18 +8,18 @@ import RoleModel from "../src/2-models/role-model";
 describe("testing auth routes", ()=>{
 
     it("should register test user", async ()=>{
-        const response = await supertest(app.server).post("/api/register").send({ firstName: 'test', lastName: 'test', email: 'test@gmail.com', password: 'hardpass', roleId: RoleModel.User})
+        const response = await supertest(app.sslServer).post("/api/register").send({ firstName: 'test', lastName: 'test', email: 'test@gmail.com', password: 'hardpass', roleId: RoleModel.User})
         const token = response.body;
         expect(token).to.be.not.empty;
     });
 
     it("should register existing admin", async ()=>{
-        const response = await supertest(app.server).post("/api/register").send({ firstName: 'admin', lastName: 'main', email: 'admin@gmail.com', password: 'hardpass', roleId: RoleModel.Admin})
+        const response = await supertest(app.sslServer).post("/api/register").send({ firstName: 'admin', lastName: 'main', email: 'admin@gmail.com', password: 'hardpass', roleId: RoleModel.Admin})
         expect(response).to.haveOwnProperty("status", 400);
     });
 
     it("should sign in", async ()=>{
-        const response = await supertest(app.server).post("/api/login").send({ email: 'admin@gmail.com', password: 'hardpass' })
+        const response = await supertest(app.sslServer).post("/api/login").send({ email: 'admin@gmail.com', password: 'hardpass' })
         const token = response.body;
         expect(token).to.be.not.empty;
     });
