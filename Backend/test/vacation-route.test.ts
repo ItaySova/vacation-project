68e6185424e765682ed1cc9ff436057f5bd6ad37
Itay <itay3742@gmail.com>
2023-09-18 18:25:30 +0300

    Update vacation-route.test.ts

--- Backend/test/vacation-route.test.ts
+++ Backend/test/vacation-route.test.ts
@@ -8,12 +8,12 @@ describe("testing vacations routes", ()=>{
     let token = null
 
     before(async () => {
-        const response = await supertest(app.server).post("/api/login").send({ email: 'admin@gmail.com', password: 'hardpass' })
+        const response = await supertest(app.sslServer).post("/api/login").send({ email: 'admin@gmail.com', password: 'hardpass' })
         token = response.body;
       });
 
     it("should return array of vacations", async () => {
-        const response = await supertest(app.server).get("/api/vacations").set('Authorization', 'Bearer ' + token)
+        const response = await supertest(app.sslServer).get("/api/vacations").set('Authorization', 'Bearer ' + token)
         const vacations = JSON.parse(response.text);
         expect(vacations.vacations.length).to.be.greaterThanOrEqual(1);
         expect(vacations.numOfPages).to.be.greaterThanOrEqual(1);
